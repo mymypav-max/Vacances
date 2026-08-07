fdbbc9f0698209043edd1dca077ed627859ebb29
mymypav-max <mymy.pav@gmail.com>
2026-08-07 17:45:55 +0200

    Add files via upload

---
+++ duel/sw-duel.js
@@ -0,0 +1,15 @@
+/* Service worker de Duel, portee limitee a son dossier */
+const CACHE = 'duel-v2';
+const FICHIERS = ['./', './duel.html', './duel.webmanifest', './icon-duel.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.all(FICHIERS.map(f => c.add(f).catch(()=>{})))).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const c = res.clone(); caches.open(CACHE).then(x => x.put(e.request, c)).catch(()=>{}); return res;
+  }).catch(() => caches.match('./duel.html'))));
+});
