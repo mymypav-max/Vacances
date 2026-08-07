70b1bb89240d87a5db07d499b0f472e7801a9562
mymypav-max <mymy.pav@gmail.com>
2026-08-07 18:13:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,12 @@
-/* Service worker commun : accueil, La Grue et Cap au Sud */
-const CACHE = 'vacances-v10';
-const FICHIERS = [
-  './',
-  './index.html',
-  './la-grue.html',
-  './la-grue.webmanifest',
-  './icon-512.png',
-  './cap-au-sud.html',
-  './cap-au-sud.webmanifest',
-  './icon-trajet.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => Promise.all(FICHIERS.map(f => c.add(f).catch(()=>{}))))
-      .then(() => self.skipWaiting())
-  );
-});
-
+/* Ancien service worker de la racine : il se desinstalle et vide ses caches.
+   Chaque jeu possede desormais le sien dans son propre dossier. */
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(cles => Promise.all(cles.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  if(e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(rep => rep || fetch(e.request).then(res => {
-      const copie = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copie)).catch(()=>{});
-      return res;
-    }).catch(() => caches.match('./index.html')))
-  );
+  e.waitUntil((async () => {
+    const cles = await caches.keys();
+    await Promise.all(cles.map(k => caches.delete(k)));
+    await self.registration.unregister();
+    const fenetres = await self.clients.matchAll({ type: 'window' });
+    fenetres.forEach(f => f.navigate(f.url));
+  })());
 });
